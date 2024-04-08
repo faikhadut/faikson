90ba5a5a63a653c68cb697a68774fdd6fc240769
faikhadut <156666285+faikhadut@users.noreply.github.com>
2024-04-08 10:03:58 +0200

    fe
    dk2

---
+++ file2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using MyCppStruct = struct 
+{
+    int16_t a = 0;
+    float b = 0.0F;
+
+    void print() const
+    {
+
+        std::cout <<"a = " << a <<"b = " << b << std::endl;
+    }
+};
+
+
+
+
+auto main() -> int 
+{
+
+MyCppStruct my_struct;
+std::cout <<"Printing struct my_struct" << std::endl;
+my_struct.print();
+}
